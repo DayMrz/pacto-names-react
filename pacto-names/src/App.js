30966fdac25de0fbff200a3af3c78cc35a4a16fe
Leily <leilymrz@gmail.com>
2022-06-03 13:41:36 -0700

    need to style :)

--- pacto-names/src/App.js
+++ pacto-names/src/App.js
@@ -1,29 +1,40 @@
-// componente que pinte las letras
-// component que pinte las letras del input con el estilo
+import React, { useState } from 'react';
 
-function Letters() {
+function Letters(props) {
+
+  const letters = props.letters.split('')
+  
   return (
     <div className='letters'>
-      <span>P</span>
-      <span>A</span>
-      <span>C</span>
-      <span>T</span>
-      <span>O</span>
+      {letters.map((letter, index) => {
+        const type = letter === ' ' ? 'div' : 'span'
+        const Container = ({children, ...props}) => React.createElement(type, props, children)
+        const variation = letter === 'i' ? 'small' : undefined
+
+        return <Container key={`${index}-${letter}`} className={variation}>{letter}</Container>
+      })}
     </div>
   );
 }
 
 function App() {
+  const [name, setName] = useState('');
+  
   return (
     <div className="App">
       <label>
         Name:
         <input
           type='text'
+          placeholder='Name'
+          onChange= {e => 
+            setName(e.target.value)
+          }
+          value={name}
         />
       </label>
 
-      <Letters />
+      <Letters letters={name} />
     </div>
   );
 }
