f36bb3a03360fa89ad08f47f94f003c20411c79f
Leily <leilymrz@gmail.com>
2022-06-03 12:37:16 -0700

    coloring letters

--- pacto-names/src/App.js
+++ pacto-names/src/App.js
@@ -2,7 +2,15 @@
 // component que pinte las letras del input con el estilo
 
 function Letters() {
-  return <div className='letters'>PACTO</div>
+  return (
+    <div className='letters'>
+      <span>P</span>
+      <span>A</span>
+      <span>C</span>
+      <span>T</span>
+      <span>O</span>
+    </div>
+  );
 }
 
 function App() {
