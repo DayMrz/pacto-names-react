f1b04221c76f5bfd33d9945e050108f3885b4a22
Leily <leilymrz@gmail.com>
2022-06-05 10:58:56 -0700

    styling footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,11 @@ import logo from '../images/pacto-historico.png'
 function Footer() {
   return (
     <footer className='footer'>
-      <img src={logo} class='logo' alt='pacto-historico logo' />
-      <p> </p>
+      <div className='img-box'>
+        <img src={logo} class='logo' alt='pacto-historico logo' />
+      </div>
+      <span className='footer-info'>Developed By DayMrz ✌</span>
+      <span className='footer-info'>https://twitter.com/DayMrz_</span>
     </footer>
   )
 }
